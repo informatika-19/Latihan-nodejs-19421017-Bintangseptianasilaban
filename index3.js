6ae86ac3d3206621959fe13b4af03590252d5dbd
bintangseptianasilaban <bintangssilaban@gmail.com>
2021-04-02 11:15:15 +0700

    first commit

---
+++ index3.js
@@ -0,0 +1,11 @@
+//Object
+const dataMahasiswa = {
+    nama: 'cahya dian indah sari',
+    npm: '19421070',
+    Alamat : {
+        rt : '01',
+        rw : '04'
+    }
+}
+
+console.log(dataMahasiswa.Alamat.rt)
